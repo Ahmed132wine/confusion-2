307de2a338bb4c5bde71eebe7a000cd40e6337e7
Efim Egorov <efim.egorov@student.saedubai.com>
2025-03-30 02:24:43 +0400

    fixes and added Escape menu to quit

--- Assets/Scripts/WinScript.cs
+++ Assets/Scripts/WinScript.cs
@@ -5,7 +5,26 @@ using UnityEngine.SceneManagement;
 public class WinScript : MonoBehaviour
 {
     [SerializeField] private GameObject nextLevelButton;
+    [SerializeField] private GameObject text;
     public GameObject winPanel;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(winPanel.activeInHierarchy == false)
+            {
+                winPanel.SetActive(true);
+                text.SetActive(false);
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                winPanel.SetActive(false);
+                Time.timeScale = 1f;
+            }
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -17,6 +36,7 @@ public class WinScript : MonoBehaviour
             }
             
             winPanel.SetActive(true);
+            text.SetActive(true);
             Time.timeScale = 0f; // Freeze the game
         }
     }
